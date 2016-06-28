424ba296ccdc975b431d2314185c84c1fdafa44e
Annichka <aakhv13@freeuni.edu.ge>
2016-06-29 00:39:12 +0400

    Quiz edit (Project commit Update)

--- Quiz/WebContent/picture.jsp
+++
@@ -1,8 +0,0 @@
-
-<div class="form">
-	<form action="#" method="post">
-		<input type="text" placeholder="Write picture url.." name="quest" /><br>
-		<input type="text" placeholder="Correct answer.." name="answ" />
-		<button> Submit </button>
-	</form>
-</div>
--- Quiz/src/connection/MyDBInfo.java
+++ Quiz/src/connection/MyDBInfo.java
@@ -2,6 +2,6 @@ package connection;
 
 public class MyDBInfo {
 	public static final String MYSQL_USERNAME = "root";
-	public static final String MYSQL_PASSWORD = "mesalo";
+	public static final String MYSQL_PASSWORD = "Sandro";
 	static final String MYSQL_DATABASE_SERVER = "jdbc:mysql://localhost:3306/WebData";
 }
